858ea49a104489b1ad9fc98b8c6d83e58d8d70b1
Victoria Parry <vlparry@gmail.com>
2022-10-20 21:30:35 -0400

    commit tree 99d05f093de65bddf47b08c77cf7a91107939b7f

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
   @@artists = []
 
   def initialize
@@ -19,13 +20,7 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+ 
 
   def add_song(song)
     @songs << song
